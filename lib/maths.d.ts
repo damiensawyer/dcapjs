a60ea46a3ade7aafe58bf637f2cd8c16f98aae05
Damien Sawyer <damiensawyer@gmail.com>
2021-04-24 00:13:42 +1000

    build

--- lib/maths.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Takes two numbers and returns their sum
- * @param a first input to sum
- * @param b second input to sum
- * @returns sum of a and b
- */
-export declare function sum(a: number, b: number): number;
-export declare function subtract(a: number, b: number): number;
